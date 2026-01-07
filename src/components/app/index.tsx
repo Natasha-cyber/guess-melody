def463a46967b242d7bb195df2cc4e71b040d967
Наталья Бреус <natabreus@MacBook-Air-von-Natala.local>
2026-01-07 21:33:54 +0300

    Добавляет маршрутизацию

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -1,4 +1,13 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+import NotFoundScreen from '../../pages/not-found-screen';
 import WelcomeScreen from '../../pages/welcome-screen';
+import AuthScreen from '../../pages/auth-screen';
+import GameOverScreen from '../../pages/game-over-screen';
+import ArtistQuestionScreen from '../../pages/artist-question-screen';
+import GenreQuestionScreen from '../../pages/genre-question-screen';
+import WinScreen from '../../pages/win-screen';
+import {AppRoute} from '../../const';
 
 type AppScreenProps = {
     errorsCount: number;
@@ -6,7 +15,17 @@ type AppScreenProps = {
 
 function App({errorsCount}: AppScreenProps) {
     return (
-        <WelcomeScreen errorsCount={errorsCount} />
+        <BrowserRouter>
+            <Routes>
+                <Route path={AppRoute.Root} element={<WelcomeScreen errorsCount={errorsCount}/>}/>
+                <Route path={AppRoute.Result} element={<WinScreen/>}/>
+                <Route path={AppRoute.DevGenre} element={<GenreQuestionScreen/>}/>
+                <Route path={AppRoute.DevArtist} element={<ArtistQuestionScreen/>}/>
+                <Route path={AppRoute.Lose} element={<GameOverScreen/>}/>
+                <Route path={AppRoute.Login} element={<AuthScreen/>}/>
+                <Route path="*" element={<NotFoundScreen/>}/>
+            </Routes>
+        </BrowserRouter>
     )
 }
 
